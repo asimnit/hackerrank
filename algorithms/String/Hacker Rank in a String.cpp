f05721e26e2b3d88dee202c8b3fd7ffd0945f32f
ASIM BERA <46041809+asimnit@users.noreply.github.com>
2019-06-19 12:09:19 +0530

    Create Hacker Rank in a String.cpp
    https://www.hackerrank.com/challenges/hackerrank-in-a-string/problem

---
+++ algorithms/String/Hacker Rank in a String.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the hackerrankInString function below.
+string hackerrankInString(string s) {
+    string str = "hackerrank";
+    int j = 0;
+    for(auto c:s)
+    {
+        if(j < str.length() && c == str[j])
+        {
+              j++;
+        }
+        if(j == str.length())
+        return "YES";
+    }
+    cout<<j;
+    return "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = hackerrankInString(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
